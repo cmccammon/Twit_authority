28e11ff95fb02d34841f0a303e18051f147921fb
Chris McCammon <cmccammon@gmail.com>
2015-09-30 14:31:37 -0500

    initial commit, ran scaffold and started adding authentication

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+
+  resources :tweets
+  resources :users
+
+  get  '/login', to: 'sessions#new',   as: 'new_login'
+  post '/login', to: 'sessions#create', as: 'login'
+  delete '/logout', to: 'sessions#destroy', as: 'logout'
+
+  root 'tweets#show'
+end
